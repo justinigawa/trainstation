92b6e738cd21ba3e345cd9ee596542693a9266bc
Justin Igawa <justinigawa13@yahoo.com>
2016-08-17 11:25:03 -0700

    fixed problems

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -12,12 +12,8 @@
 // Create a variable to reference the database
 var database = firebase.database();
 
-// Initial Values
-var trainName = "";
-var destination = "";
-var frequency = 0;
-var nextArrival = 0;
-var minTil = 0;
+// Initial Values var trainName = ""; var destination = ""; var frequency = 0;
+var nextT = 0; var minTil = 0;
 
 
 $('#submit').on('click', function() {
@@ -59,14 +55,15 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
     var $name = $("<h4 id='nameDisplay'>" + childSnapshot.val().name + "</h4>");
     var $dest = $("<h4 id='destDisplay'>" + childSnapshot.val().dest + "</h4>");
     var $freq = $("<h4 id='freqDisplay'>" + childSnapshot.val().freq + "</h4>");
-	var $firstT = $("<h4 id='firstTDisplay'>" + childSnapshot.val().firstT + "</h4>");
-	/*var tFrequency = childSnapshot.val().freq;
+	var $nextT = $("<h4 id='nextTDisplay'>" + childSnapshot.val().nextT + "</h4>");
+	var $minTil = $("<h4 id='minTilDisplay'>" + childSnapshot.val().minTil + "</h4>");
+	
+	var tFrequency = childSnapshot.val().freq;
 	var firstTime = childSnapshot.val().firstT;
 
 	//first time (pushed back 1 year to make sure it comes before current time)
 	var firstTimeConverted = moment(firstTime, "hh:mm").subtract(1,"years");
 	console.log(firstTimeConverted);
-
 
 	//current time
 	var currentTime= moment();
@@ -84,29 +81,29 @@ database.ref().on("child_added", function(childSnapshot, prevChildKey) {
 	var tMinutesTillTrain = tFrequency - tRemainder;
 	console.log("MINUTES TILL TRAIN: " + tMinutesTillTrain);
 
-	//next train
+	//next trainx
 	var nextTrain = moment().add(tMinutesTillTrain, "minutes");
-	console.log("ARRIVAL TIME: "+ moment(nextTrain).format("hh:mm"));*/
+	console.log("ARRIVAL TIME: "+ moment(nextTrain).format("hh:mm"));
 
-       
+         
 
 	   
-       $("#nameDisplay").append($div).append($name);
-	   $("#destDisplay").append($div).append($dest);
-	   $("#freqDisplay").append($div).append($freq);
-	   
-	   
 
-// Handle the errors
+	// Change the HTML to reflect
+	$("#newEntry").append("<tr><br><td>" + childSnapshot.val().name + "</td><br><td>" + childSnapshot.val().dest + "</td><br><td>" + childSnapshot.val().freq + "</td><br><td>" + moment(nextTrain).format("hh:mm") + "</td><br><td>" + tMinutesTillTrain + "</td>");
+})
+
+
+
+database.ref().orderByChild("dateAdded").limitToLast(7).on("child_added", function(childSnapshot){
+
+	// Handle the errors
 }, function(errorObject){
 
     console.log("Errors handled: " + errorObject.code)
 });
 
-database.ref().orderByChild("dateAdded").limitToLast(7).on("child_added", function(childSnapshot){
-	// Change the HTML to reflect
-	$("#newEntry").append("<tr><br><td>" + childSnapshot.val().name + "</td><br><td>" + childSnapshot.val().dest + "</td><br><td>" + childSnapshot.val().freq + "</td><br><td></td><td>");
-})
+
 
 
 
